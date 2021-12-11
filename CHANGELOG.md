9897a935f644e8cff59fe7e8c94a1189c0f3062c
crimix <christofferm@live.dk>
2021-12-11 15:14:06 +0100

    Finish port to 1.17

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,8 @@
 # Armor Unlocked Changelog
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
-## [1.17.1, 1.0.1-b1] - 2021-08-xx
+## [1.17.1, 1.0.1] - 2021-12-11
 - Port to 1.17.1
-- Will be in beta until full Forge release, expect things to break.
 
 ## [1.16.3, 1.0.1] - 2021-01-02
 - Fix mods.toml not having sides both.
--- src/main/java/com/black_dog20/armorunlocked/mixin/CombatRulesMixin.java
+++ src/main/java/com/black_dog20/armorunlocked/mixin/CombatRulesMixin.java
@@ -1,22 +1,22 @@
-//package com.black_dog20.armorunlocked.mixin;
-//
-//import com.black_dog20.armorunlocked.Config;
-//import net.minecraft.util.CombatRules;
-//import net.minecraft.util.math.MathHelper;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-//
-//import static org.spongepowered.asm.mixin.injection.callback.LocalCapture.*;
-//
-//@Mixin(CombatRules.class)
-//public class CombatRulesMixin {
-//
-//    @Inject(method = "getDamageAfterAbsorb(FFF)F", at = @At("RETURN"), cancellable = true, locals = CAPTURE_FAILSOFT)
-//    private static void getDamageAfterAbsorb(float damage, float totalArmor, float toughnessAttribute, CallbackInfoReturnable<Float> ci, float f) {
-//        float max = Config.FULL_UNLOCKED.get() ? 25F : 23.75F;
-//        float f1 = MathHelper.clamp(totalArmor - damage / f, totalArmor * 0.2F,  max);
-//        ci.setReturnValue(damage * (1.0F - f1 / 25.0F));
-//    }
-//} //TODO When mixins are back
+package com.black_dog20.armorunlocked.mixin;
+
+import com.black_dog20.armorunlocked.Config;
+import net.minecraft.world.damagesource.CombatRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.minecraft.util.Mth.clamp;
+import static org.spongepowered.asm.mixin.injection.callback.LocalCapture.CAPTURE_FAILSOFT;
+
+@Mixin(CombatRules.class)
+public class CombatRulesMixin {
+
+    @Inject(method = "getDamageAfterAbsorb(FFF)F", at = @At("RETURN"), cancellable = true, locals = CAPTURE_FAILSOFT)
+    private static void getDamageAfterAbsorb(float damage, float totalArmor, float toughnessAttribute, CallbackInfoReturnable<Float> ci, float f) {
+        float max = Config.FULL_UNLOCKED.get() ? 25F : 23.75F;
+        float f1 = clamp(totalArmor - damage / f, totalArmor * 0.2F,  max);
+        ci.setReturnValue(damage * (1.0F - f1 / 25.0F));
+    }
+}
